df1432175938701a0ea57376af71fa5292a9d947
Sardonyx001 <lassouedjamel@hotmail.fr>
2023-08-25 00:59:27 +0900

    Removed unnecesary

--- drizzle/0000_harsh_ironclad.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `todos` (
-	`id` integer PRIMARY KEY NOT NULL,
-	`content` text,
-	`done` integer
-);
---> statement-breakpoint
-CREATE UNIQUE INDEX `todos_id_unique` ON `todos` (`id`);
